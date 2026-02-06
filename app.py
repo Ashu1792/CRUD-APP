2d8ca4a5ff80dd435be8871884fef2a3a3a94bea
Ashu1792 <ashupal112200@gmail.com>
2026-02-06 11:02:54 +0530

    add contact page'

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# REQUIRED for flash messages
+app.secret_key = "mysecretkey"
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Employee.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -40,17 +43,18 @@ def add_employee():
 @app.route("/about")
 def about():
     return render_template("about.html")
-# ------------------ Delete ------------------
 
+
+# ------------------ DELETE ------------------
 @app.route("/delete/<int:id>")
 def delete(id):
     emp = Employee.query.get_or_404(id)
     db.session.delete(emp)
     db.session.commit()
     return redirect(url_for("home"))
-#------------------Update------------------
 
 
+# ------------------ UPDATE ------------------
 @app.route("/update/<int:id>", methods=["GET", "POST"])
 def update(id):
     emp = Employee.query.get_or_404(id)
@@ -64,11 +68,25 @@ def update(id):
     return render_template("update.html", emp=emp)
 
 
+# ------------------ CONTACT ------------------
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        subject = request.form["subject"]
+        message = request.form["message"]
+
+        print(name, email, subject, message)
+
+        flash("Message sent successfully!")
+        return redirect(url_for("home"))
+
+    return render_template("contact.html")
 
 
-
+# ------------------ MAIN ------------------
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-
