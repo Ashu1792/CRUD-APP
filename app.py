031d2529580e0a2352786405b4001ba20f6c6f47
Ashu1792 <ashupal112200@gmail.com>
2026-02-10 15:11:13 +0530

    fix some errors

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Employee.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
+
 
 
 # ------------------ MODEL ------------------
